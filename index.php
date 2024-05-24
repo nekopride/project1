6a75c3ea64660a208d4fd995524c8904a73e7acb
OPPabcd <158025334+OPPabcd@users.noreply.github.com>
2024-05-24 18:54:33 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
+include 'config.php';
 session_start();
 print_r($_SESSION);
+$sql_users = "SELECT COUNT(*) as total_users FROM user"; // Query untuk menghitung jumlah pengguna
+$result_users = $connect->query($sql_users);
+
+$total_users = 0;
+if ($result_users->num_rows > 0) {
+    while($row_users = $result_users->fetch_assoc()) {
+        $total_users = $row_users["total_users"];
+    }
+}
+
+// Query untuk mengambil jumlah barang
+$sql_items = "SELECT COUNT(*) as total_items FROM barang"; // Query untuk menghitung jumlah barang
+$result_items = $connect->query($sql_items);
+
+$total_items = 0;
+if ($result_items->num_rows > 0) {
+    while($row_items = $result_items->fetch_assoc()) {
+        $total_items = $row_items["total_items"];
+    }
+}
+
+// Menutup koneksi
+$connect->close();
 ?>
 <!DOCTYPE html>
 <html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
@@ -651,7 +675,7 @@ print_r($_SESSION);
                   <p
                     class="text-lg font-semibold text-gray-700 dark:text-gray-200"
                   >
-                    5000
+                  <?php echo $total_items; ?>
                   </p>
                 </div>
               </div>
@@ -662,24 +686,26 @@ print_r($_SESSION);
                 <div
                   class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500"
                 >
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="10" cy="6" r="3" fill-rule="evenodd" clip-rule="evenodd"></circle>
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 9a5 5 0 00-5 5v2h10v-2a5 5 0 00-5-5z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+
                 </div>
                 <div>
                   <p
                     class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
                   >
-                    Pending contacts
+                    Jumlah User
                   </p>
                   <p
                     class="text-lg font-semibold text-gray-700 dark:text-gray-200"
                   >
-                    35
+                  <?php echo $total_users; ?>
                   </p>
                 </div>
               </div>
