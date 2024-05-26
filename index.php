a8f8b0079876123c179682da8d8580a25d916f34
pamond <158030169+pamond@users.noreply.github.com>
2024-05-26 12:37:31 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ $connect->close();
               ></span>
               <a
                 class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                href="index.html"
+                href="index.php"
               >
                 <svg
                   class="w-5 h-5"
@@ -259,7 +259,7 @@ $connect->close();
               ></span>
               <a
                 class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                href="index.html"
+                href="index.php"
               >
                 <svg
                   class="w-5 h-5"
