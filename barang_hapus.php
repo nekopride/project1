e732d31d21627526d4ca49f20c725b62c6f20588
nekopride <158025518+nekopride@users.noreply.github.com>
2024-05-29 18:49:22 +0700

    Add files via upload

--- barang_hapus.php
+++ barang_hapus.php
@@ -2,10 +2,17 @@
 include 'config.php';
 
 if (isset($_GET['id'])){
-     $id = $_GET['id'];
+    $id = $_GET['id'];
 
-     mysqli_query($connect, " DELETE FROM barang WHERE id_barang='$id' ");
-     header("location:tambah_barang.php");
+    $query = "DELETE FROM barang WHERE id_barang='$id'";
+    $result = mysqli_query($connect, $query);
+
+    if ($result) {
+        header("location:tambah_barang.php");
+    } else {
+        error_log("Error menghapus barang dengan ID $id: " . mysqli_error($connect));
+        $_SESSION['error'] = "Gagal menghapus barang: " . mysqli_error($connect);
+        header("location:tambah_barang.php");
+    }
 }
-?>
 ?>
