45afd3cbb6ea35d998585312c0ea0f8e717a7836
OPPabcd <158025334+OPPabcd@users.noreply.github.com>
2024-05-30 11:05:43 +0700

    Update barang_masuk.php

--- barang_masuk.php
+++ barang_masuk.php
@@ -126,6 +126,14 @@ $result_masuk = $connect->query($query_masuk);
                     </a>
                 </li>
             </ul>
+                    <div class="px-6 my-6">
+                        <a href="pages/user_add.php">
+                    <button
+                      class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                      Create account
+                      <span class="ml-2" aria-hidden="true">+</span>
+                    </button>
+                  </div>
         </div>
     </aside>
     <div class="flex flex-col flex-1 w-full">
